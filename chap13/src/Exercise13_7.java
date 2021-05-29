1c4b24bca43f79ca44815955db896d9735f3b5c1
iw0406 <hyunjilim98@gmail.com>
2021-05-29 20:20:36 +0900

    chap13 - 쓰레드

---
+++ chap13/src/Exercise13_7.java
@@ -0,0 +1,29 @@
+public class Exercise13_7 {
+    static  boolean stopped = false;
+
+    public static void main(String[] args) {
+        Thread5 th1 = new Thread5();
+        th1.start();
+
+        try{
+            Thread.sleep(6*10000);
+        } catch (InterruptedException e) { }
+
+        stopped = true;
+        th1.interrupt();
+        System.out.println("stopped");
+    }
+}
+
+class Thread5 extends Thread{
+
+    @Override
+    public void run() {
+        for(int i=0;!Exercise13_7.stopped;i++){
+            System.out.println(i);
+            try {
+                Thread.sleep(3*1000);
+            } catch (InterruptedException e) { }
+        }
+    }
+}
